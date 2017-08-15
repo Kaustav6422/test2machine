414f9d706c383123845ca6f3b180bb6ff658e2d2
snailwoo <woojingran@outlook.com>
2017-08-15 17:15:30 +0800

    add files

---
+++ env/robot_setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+export monitor=192.168.7.15
+export robot=192.168.7.2
+
+export ROS_IP=192.168.7.2
+export ROS_MASTER_URI=http://192.168.7.15:11311
+export ROSLAUNCH_SSH_UNKNOWN=1
+
+source ~/.bashrc
+source /opt/ros/kinetic/setup.bash
+source ~/catkin_ws/devel/setup.bash
+exec "$@"
